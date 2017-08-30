85899b42eae67ca58784618d0956021b3de51260
Andrew Johnson <ajj@byu.net>
2017-08-30 12:37:31 -0500

    Adds checkboxes to page

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
-alert("On the Economist Print Edition page.");
+var i = 0;
+// Make a parent wrapping element for each story
+$(".list__link").wrap("<div class='ajj-link-wrap'></div>");
+$(".ajj-link-wrap").prepend("<input type='checkbox' style='position: absolute; margin-left: -10px;' name='ajj-checkbox'>");
+
+var stories = document.getElementsByClassName("ajj-link-wrap");
+
+for (var i = 0; i < stories.length; i++) {
+}
