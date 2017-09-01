58db7f2eda65cae157d98bba55159d4432c3d1da
Andrew Johnson <ajj@byu.net>
2017-09-01 13:35:21 -0500

    Checkboxes update, just don't store properly

--- content.js
+++ content.js
@@ -1,38 +1,68 @@
 
 function ajjClickCB() {
-    var keyname = "week-2017-08-26-no-" + this.attributes["name"].value;
-    var currentid = "ajj-checkbox-" + this.attributes["name"].value;
-    var currentbox = document.getElementById(currentid);
+    console.log("CLICK");
     var input = {};
-    input[keyname] = currentbox.checked;
-    chrome.storage.local.set(input, function(){
-        console.log(keyname + " " + currentbox.checked);
-        chrome.storage.local.get(keyname, function(items){
-            console.log(items);
+    var currentname = this.attributes["name"].value;
+    var currentid = "ajj-checkbox-" + currentname;
+    var currentbox = document.getElementById(currentid);
+    console.log(currentid + " " + currentbox.checked);
+
+    chrome.storage.local.get([url], function(items){
+        console.log(items);
+        console.log(`
+        ^^^ these are items
+        
+        `);
+        items[currentname] = currentbox.checked;
+        console.log(items[currentname]);
+        input[url] = items;
+        chrome.storage.local.set(input, function(){
+            console.log(currentname + " is " + currentbox.checked);
+            chrome.storage.local.get([url], function(items2){
+                console.log(items2);
+                console.log(`
+                
+                
+                `);
+            });
         });
     });
 }
 
-// Make a parent wrapping element for each story
+
+var url = location.href.replace('https://www.economist.com/printedition/','');
 $(".list__link").wrap("<div class='ajj-link-wrap'></div>");
 $(".ajj-link-wrap").prepend("<input type='checkbox' class='ajj-checkbox' style='position: absolute; margin-left: -10px;'>");
-
-chrome.storage.local.get(["week-2017-08-26"], function(items){
-    console.log(items);
-});
-
 var stories = document.getElementsByClassName("ajj-link-wrap");
 
-
-
-for (var i = 0; i < stories.length; i++) {
-    stories[i].firstChild.setAttribute("id", "ajj-checkbox-" + i);
-    stories[i].firstChild.setAttribute("name", i);
-    stories[i].firstChild.addEventListener('click', ajjClickCB);
-}
-
-chrome.storage.local.get(["date"], function(items){
-    console.log(items);
+chrome.storage.local.get([url], function(items){
+    if (typeof items !== 'array') {
+        var items = [];
+        for (var i = 0; i < stories.length; i++) {
+            items.push(false);
+        }
+        var input = {};
+        input[url] = items;
+        chrome.storage.local.set(input, function(){
+            /*chrome.storage.local.get([url], function(items){
+                console.log("successfully logged: ");
+                console.log(items);
+            });*/
+        });
+    }    
+    items[0] = true;
+    var ouritems = items;
+    console.log(ouritems);
+    for (var i = 0; i < stories.length; i++) {
+        stories[i].firstChild.setAttribute("id", "ajj-checkbox-" + i);
+        stories[i].firstChild.setAttribute("name", i);
+        if (items[i] === true) {
+            stories[i].firstChild.setAttribute("checked", "checked");
+        }
+        stories[i].firstChild.addEventListener('click', ajjClickCB);
+    }
 });
+
+
 
 
