0493adb8a57d0a79cbd825b44a37a2ed6f693bbe
Andrew Johnson <ajj@byu.net>
2017-09-04 22:39:12 -0500

    Refactored everything

--- content.js
+++ content.js
@@ -1,85 +1,48 @@
 
 function ajjClickCB() {
     console.log("CLICK");
-    var input = {};
-    var currentname = this.attributes["name"].value;
-    var currentid = "ajj-checkbox-" + currentname;
-    var currentbox = document.getElementById(currentid);
-    console.log(currentid + " " + currentbox.checked);
+    var currentNumber = this.attributes["name"].value;
+    var currentCheckbox = document.getElementById("ajj-checkbox-" + currentNumber);
 
-    chrome.storage.local.get([url], function(items){
+    chrome.storage.local.get(urlDate, function(items){
         console.log(items);
         console.log(`
         ^^^ these are items
         
         `);
-        items[currentname] = currentbox.checked;
-        console.log(items[currentname]);
-        input["checks"] = items;
-        chrome.storage.local.set(input, function(){
-            console.log(currentname + " is " + currentbox.checked);
-            chrome.storage.local.get([url], function(items2){
-                console.log(items2);
-                console.log(`
-                
-                
-                `);
-            });
-        });
+        items[urlDate][currentNumber] = currentCheckbox.checked;
+        chrome.storage.local.set(items, function(){ });
     });
 }
 
 
-var url = location.href.replace('https://www.economist.com/printedition/','');
+var urlDate = location.href.replace('https://www.economist.com/printedition/','');
 $(".list__link").wrap("<div class='ajj-link-wrap'></div>");
 $(".ajj-link-wrap").prepend("<input type='checkbox' class='ajj-checkbox' style='position: absolute; margin-left: -10px;'>");
 var stories = document.getElementsByClassName("ajj-link-wrap");
 
-chrome.storage.local.get(url, function(items){
-    console.log(`
-    
-    ITEMS RETRIEVED AT ` + url + `:
-    `);
-    console.log(items[url]);
-    var checks = items[url];
-        var checks = [];
-        items.checks[0] = true;
-        items.checks[1] = false;
-    console.log(items);
-    items = {};
-    if (typeof items.checks === 'undefined') {
-        items.checks = [];
+chrome.storage.local.get(urlDate, function(items){
+
+    // If the data stored at this date is not an array, make it an array and populate it with spots for each story for this date, then upload it.
+    // Instantiates the data on the first visit.
+    if (Object.prototype.toString.call( items[urlDate] ) !== '[object Array]') {
+        items[urlDate] = [];
         for (var i = 0; i < stories.length; i++) {
-            items.checks.push(false);
+            items[urlDate].push(false);
         }
-    }
-    console.log(`here's what we'll set it up with:`);
-    console.log(url);
-    newItems = {};
-    newItems[url] = items.checks;
-    console.log(newItems); 
-//    console.log(items);
-    if (typeof items === 'array') {
-        var items = [];
-        for (var i = 0; i < stories.length; i++) {
-            items.push(false);
-        }
-        var input = {};
-        input[url] = items;
-        chrome.storage.local.set(input, function(){
-            /*chrome.storage.local.get([url], function(items){
-                console.log("successfully logged: ");
-                console.log(items);
-            });*/
+        chrome.storage.local.set(items, function(){
+            console.log("successfully uploaded the following data:");
+            console.log(items);
         });
-    }    
-    items[0] = true;
-    var ouritems = items;
-    console.log(ouritems);
+    }
+
+    // Now populate the page with checkboxes according to the data we've retrieved
+
+    
     for (var i = 0; i < stories.length; i++) {
         stories[i].firstChild.setAttribute("id", "ajj-checkbox-" + i);
         stories[i].firstChild.setAttribute("name", i);
-        if (items[i] === true) {
+        if (items[urlDate][i] === true) {
             stories[i].firstChild.setAttribute("checked", "checked");
         }
         stories[i].firstChild.addEventListener('click', ajjClickCB);
